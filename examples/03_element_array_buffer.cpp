926ece2877ff7beff2a6ed0a4268ec84e753cf95
Sruthik P <psruthik@gmail.com>
2019-07-23 19:32:08 +0530

    Added EBO Example

---
+++ examples/03_element_array_buffer.cpp
@@ -0,0 +1,97 @@
+#include "utils/native_window.hpp"
+#include "utils/opengl_context.hpp"
+#include "utils/opengl_shader_utils.hpp"
+#include "utils/math/template_math_ops.hpp"
+
+const float vertexData[] = {
+    -0.5f, -0.5f, 0.0f,
+    0.5f, -0.5f, 0.0f,
+    0.5f, 0.5f, 0.0f,
+    -0.5f, 0.5f, 0.0f
+};
+
+const int indices[] = {
+	0, 1, 2,
+	2, 3, 0
+};
+
+const char *vertexSource =
+    "#version 450 core\n"
+    "layout (location = 0) in vec3 vertexPosition;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(vertexPosition, 1.0);\n"
+    "}\0";
+const char *fragmentSource = 
+    "#version 450 core\n"
+    "out vec4 fragmentColor;\n"
+    "void main()\n"
+    "{\n"
+    "   fragmentColor = vec4(1.0, 0.0, 0.0, 0.0);\n"
+    "}\0";
+
+int main ()
+{
+	nativeWindow win(800, 600);
+	glContext gc;
+	GLuint VAO;
+	GLuint VBO;
+	GLuint EBO;
+	GLuint vtxShaderObj;
+	GLuint fragShaderObj;
+	GLuint programObj;
+
+	if (!win.createNativeWindow()) {
+        std::cout << "Failed to create native window" << std::endl;
+        return -1;
+    }
+
+    if (!gc.initializeGlContext(win, 4, 5)) {
+        std::cout << "Failed to initialize GL Context" << std::endl;
+        return -1;
+    }
+
+    vtxShaderObj = glCreateShader(GL_VERTEX_SHADER);
+    fragShaderObj = glCreateShader(GL_FRAGMENT_SHADER);
+    programObj = glCreateProgram();
+
+    if (!compileShader(vtxShaderObj, vertexSource)) {
+        std::cout << "Failed to compile GLSL Vertex Shader" << std::endl;
+        return -1;
+    }
+
+    if (!compileShader(fragShaderObj, fragmentSource)) {
+        std::cout << "Failed to compile GLSL Fragment Shader" << std::endl;
+        return -1;
+    }
+
+    glAttachShader(programObj, vtxShaderObj);
+    glAttachShader(programObj, fragShaderObj);
+
+    if (!linkShaders(programObj)) {
+        std::cout << "Failed to link GLSL Shaders" << std::endl;
+        return -1;
+    }
+
+    glUseProgram(programObj);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glEnableVertexAttribArray(0);
+    glClearColor(0.0, 1.0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    gc.swapBuffers();
+#ifdef _WIN32
+    system("pause");
+#elif defined __linux__
+    sleep(5);
+#endif
+    return 0;
+}

57293d760d5e5c0afb2f88d16bf66e4f949ad2b9
Sruthik <psruthik@gmail.com>
2021-09-20 17:18:34 +0530

    D3D12: Add comments to clear color example

--- d3d12/01_clear_color.cpp
+++ d3d12/01_clear_color.cpp
@@ -82,13 +82,23 @@ int main() {
         return -1;
     }   
 
-    uint32_t backBufferIdx = gc.pSwapchain3->GetCurrentBackBufferIndex();
-
+    ///
+    /// RTV Descriptor heap creation
+    ///
+    /// The RTV Descriptor heap creation involves the following steps:
+    ///
+    /// 1. Create the backing allocation for the descriptor heap. One descriptor per swapchain buffer.
+    ///    This is done with a call to ID3D12Device::CreateDescriptorHeap.
+    /// 2. Create Render target views for each swapchain buffer. ICDs will add VA pointers to the actual
+    ///    swapchain buffer resident in vidmem in the descriptor.
+    /// 
     D3D12_DESCRIPTOR_HEAP_DESC rtvDescHeap = {};
-    rtvDescHeap.NumDescriptors = 2;
+    rtvDescHeap.NumDescriptors = 2; // We create one descriptor for each swapchain buffer.
     rtvDescHeap.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
     rtvDescHeap.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
     ID3D12DescriptorHeap *pRtvDescHeap;
+    /// Note: RTV heaps are always allocated in sysmem. This is fine since the actual swapchain
+    ///       buffers would have already been allocated in vidmem during swapchain creation.
     ret = gc.pD3d12Device->CreateDescriptorHeap(&rtvDescHeap, IID_PPV_ARGS(&pRtvDescHeap));
     if (ret != S_OK) {
         std::cerr << "Failed to create RTV Descriptor heap\n";
@@ -102,6 +112,7 @@ int main() {
         if (ret != S_OK) {
             std::cerr << "Failed to get buffer\n";
         } else {
+            /// Fill in the descriptor with VA pointers to the swapchain buffer.
             gc.pD3d12Device->CreateRenderTargetView(pswapchainBuffers[idx].Get(), nullptr, rtvDescHandle);
             rtvDescHandle.ptr += rtvDescIncrementSize;
         }
@@ -111,6 +122,9 @@ int main() {
         return -1;
     }
 
+    ///
+    /// Create a ID3D12Fence that can be signalled by the GPU once all required work and presents are completed.
+    ///
     ComPtr<ID3D12Fence> pFence;
     uint32_t initialFenceValue = 0U;
     uint32_t signaledFenceValue = initialFenceValue + 1U;
@@ -120,7 +134,9 @@ int main() {
         return -1;
     }
 
-    // Create an unsignaled event
+    ///
+    /// Create an unsignaled event. This event is signalled once pFence is signalled by the GPU.
+    ///
     HANDLE fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
     if (fenceEvent == nullptr) {
         std::cerr << "Failed to create event\n";
@@ -129,6 +145,8 @@ int main() {
 
     float colors[4];
     colors[3] = 0.0F;
+
+    uint32_t backBufferIdx = gc.pSwapchain3->GetCurrentBackBufferIndex();
 
     while ((event = eventLoop.GetEvent()) != GLPG::GLPGEvent::Key_Escape) {
 	    colors[0] = red.operator++();
@@ -147,6 +165,9 @@ int main() {
             return -1;
         }
 
+        ///
+        /// Transition the RTV from Present to Render Target state.
+        ///
         D3D12_RESOURCE_TRANSITION_BARRIER rtvPreTransationBarrier = {
             pswapchainBuffers[backBufferIdx].Get(),
             D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES,
@@ -159,6 +180,10 @@ int main() {
         rtvPreBarrier.Transition = rtvPreTransationBarrier;
         rtvPreBarrier.Flags = D3D12_RESOURCE_BARRIER_FLAG_NONE;
 
+        ///
+        /// Insert the pre transition barrier for the RTV.
+        /// This sets the RTV state to Render Target.
+        ///
         gc.pCmdList->ResourceBarrier(1, &rtvPreBarrier);
 
         D3D12_CPU_DESCRIPTOR_HANDLE currentRtvHandle = pRtvDescHeap->GetCPUDescriptorHandleForHeapStart();
@@ -166,6 +191,9 @@ int main() {
 
         gc.pCmdList->ClearRenderTargetView(currentRtvHandle, colors, 0, nullptr);
 
+        ///
+        /// Transition the RTV from Render Target to Present state.
+        ///
         D3D12_RESOURCE_TRANSITION_BARRIER rtvPostTransationBarrier = {
             pswapchainBuffers[backBufferIdx].Get(),
             D3D12_RESOURCE_BARRIER_ALL_SUBRESOURCES,
@@ -192,13 +220,31 @@ int main() {
             return -1;
         }
         gc.pCommandQueue->ExecuteCommandLists(1U, &ppCmdList);
-
+        ///
+        /// NOTE: Most ICDs append a recorded command list with a fence signal method.
+        ///       This fence signal method signals the completion of the execution of all GPU methods
+        ///       preceeding it. The IDXGISwapChain::Present calls implicitly wait on this signal
+        ///       event before proceeding with the actual flip. Hence applications need not explicitly add
+        ///       a wait before a present call.
         ret = gc.pSwapchain3->Present(0, 0);
         if (ret != S_OK) {
             std::cerr << "Failed to present \n";
             return -1;
         }
 
+        ///
+        /// Add a signal method to the command queue. The fence is signalled by the GPU
+        /// only after all the previously queued commands have completed execution.
+        ///
+        /// By this point, the command queue would look something like below:
+        ///
+        /// 1. RTV Pre transition barrier.
+        /// 2. ClearColor method
+        /// 3. RTV Post transition barrier
+        /// 4. Signal method inserted by the GPU ICD
+        /// 5. DXGI Present. (This isn't a method submitted to the GPU. The GPU's DC which is an entirely separate block takes care of the flip.)
+        /// 6. Signal method inserted by the app below.
+        ///
         ret = gc.pCommandQueue->Signal(pFence.Get(), signaledFenceValue);
         if (ret != S_OK) {
             std::cerr << "Failed to signal fence\n";
@@ -206,13 +252,26 @@ int main() {
         }
 
         if (pFence->GetCompletedValue() < signaledFenceValue) {
+            ///
+            /// If the fence hasn't been signalled by the GPU yet (which will most probably be the case unless
+            /// the submitted commands are executed faster than the time it took for the CPU to reach this point)
+            /// attach an event to the fence. When the fence is signalled, the event is fired.
+            ///
             if (pFence->SetEventOnCompletion(signaledFenceValue, fenceEvent) != S_OK) {
                 std::cerr << "failed to set event on completion\n";
                 return -1;
             }
+            ///
+            /// Perform a CPU-wait on the event till it is fired by the GPU.
+            /// Note that we do end up stalling on the CPU here while the CPU could be used for
+            /// other useful work such as preparing the commands for the next frame etc. This isn't ideal.
+            ///
             WaitForSingleObject(fenceEvent, INFINITE);
         }
         backBufferIdx = gc.pSwapchain3->GetCurrentBackBufferIndex();
+        ///
+        /// Increment the fence value that should be waited for on the next frame.
+        ///
         signaledFenceValue++;
     }
 }
